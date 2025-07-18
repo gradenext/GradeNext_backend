2bce5cb6152371f56adbf82504ae3a56960b0d88
Kuldeepkushwah06 <144555844+Kuldeepkushwah06@users.noreply.github.com>
2025-07-18 22:48:58 +0530

    send grid updated

--- gradenext/settings.py
+++ gradenext/settings.py
@@ -270,7 +270,7 @@ EMAIL_PORT       = 2525
 EMAIL_USE_TLS    = True
 EMAIL_HOST_USER     = "apikey"
 EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY")
-DEFAULT_FROM_EMAIL  = "nextgradeinfo@gmail.com"
+DEFAULT_FROM_EMAIL  = "contact@gradenext.com"
 
 LOGGING = {
     'version': 1,
